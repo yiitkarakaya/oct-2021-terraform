0586070f33ac0d7e524fc2e0527bc690aedd5928
farrukh90 <farrukhsadykov@gmail.com>
2022-01-22 22:02:15 +0000

    added loop

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -2,12 +2,23 @@ resource "aws_iam_user" "lb" {
   name = "loadbalancer"
 }
 
+resource "aws_iam_user" "multiuser" {
+	name = each.key
+	for_each = toset([
+	"bob",
+	"sam",
+	"lisa",
+	])
+}
+
+
+
 resource "aws_iam_group" "octobergroup" {
   name = "octobergroup"
 }
 
 resource "aws_iam_group_membership" "developers_team" {
-  name = "developers-group-membership"
+  name  = "developers-group-membership"
   users = [aws_iam_user.lb.name]
   group = aws_iam_group.octobergroup.name
 }
