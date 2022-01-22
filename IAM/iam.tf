25e9f9b457072a922c984776865af5f1d171877d
farrukh90 <farrukhsadykov@gmail.com>
2022-01-22 21:55:39 +0000

    added attachment

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -5,3 +5,9 @@ resource "aws_iam_user" "lb" {
 resource "aws_iam_group" "octobergroup" {
   name = "octobergroup"
 }
+
+resource "aws_iam_group_membership" "developers_team" {
+  name = "developers-group-membership"
+  users = [aws_iam_user.lb.name]
+  group = aws_iam_group.octobergroup.name
+}
