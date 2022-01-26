0d18323f66424f0b4ee2ea45fb8a217a2cd6d545
farrukh90 <farrukhsadykov@gmail.com>
2022-01-25 19:33:42 -0600

    added 80

--- Class2/sec_group.tf
+++ Class2/sec_group.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "ec2-class-sec-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
