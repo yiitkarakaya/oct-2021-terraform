b6907ae0a23ce1185cdd0edeb59eb3466016ccb7
farrukh90 <farrukhsadykov@gmail.com>
2022-01-25 18:33:17 -0600

    classified resources

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -15,40 +15,6 @@ data "aws_ami" "ubuntu" {
 }
 
 
-resource "aws_security_group" "ec2-class-sec-group" {
-  name        = "ec2-class-sec-group"
-  description = "Allow TLS inbound traffic"
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
-
 
 
 
