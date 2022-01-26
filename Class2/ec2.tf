044dd91f36e1e503d78139ddb7f6fe8479278ab3
farrukh90 <farrukhsadykov@gmail.com>
2022-01-25 18:47:38 -0600

    added  output

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -24,4 +24,5 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.ec2-class-sec-group.id]
+  associate_public_ip_address = true
 }
---
+++ Class2/output.tf
@@ -0,0 +1,13 @@
+
+output "all" {
+  value = aws_instance.web.public_ip
+}
+
+output "ami" {
+  value = aws_instance.web.ami
+}
+
+output "az" {
+  value = aws_instance.web.availability_zone
+  sensitive = true
+}
