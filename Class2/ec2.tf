db203d0cb0266173eac99828df456fdf6891761e
farrukh90 <farrukhsadykov@gmail.com>
2022-01-25 19:31:21 -0600

    added remote exec

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -17,23 +17,52 @@ data "aws_ami" "ubuntu" {
 }
 
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.large"
-  vpc_security_group_ids = [aws_security_group.ec2-class-sec-group.id]
-  associate_public_ip_address = true
-  availability_zone = "us-east-1a"
-  tags = local.common_tags
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
+
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.large"
+  vpc_security_group_ids      = [aws_security_group.ec2-class-sec-group.id]
+  associate_public_ip_address = true
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.deployer.key_name
+  tags                        = local.common_tags
+
+
+
+  provisioner "remote-exec" {
+    connection {
+      host        = aws_instance.web.public_ip
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+    }
+    inline = [
+      "sudo apt-get install apache2 -y",
+    ]
+  }
+}
+
+
+
+
+
+
 
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 40
-  tags = local.common_tags
+  tags              = local.common_tags
 }
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.example.id
   instance_id = aws_instance.web.id
-  
+
 }
--- Class2/locals.tf
+++ Class2/locals.tf
@@ -1,8 +1,8 @@
 locals {
   common_tags = {
-      Name = "HelloWorld"
-      Env = "Dev"
-      Team = "DevOps"
-      Created_by = "DevOps"
- }
+    Name       = "HelloWorld"
+    Env        = "Dev"
+    Team       = "DevOps"
+    Created_by = "DevOps"
+  }
 }
