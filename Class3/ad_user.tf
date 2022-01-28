b5a02658d731a9476314a16ea313fbed4f3c0eae
farrukh90 <farrukhsadykov@gmail.com>
2022-01-27 18:37:40 -0600

    added user

---
+++ Class3/ad_user.tf
@@ -0,0 +1,38 @@
+
+data "azuread_domains" "aad_domains" {}
+
+
+
+output "domains" {
+	value = data.azuread_domains.aad_domains.domains[0].domain_name
+}
+
+
+
+resource "random_password" "password" {
+	length = 30
+	special = false
+	override_special = "_%@"
+}
+
+
+
+
+resource "azuread_user" "example" {
+	user_principal_name = "jdoe@${data.azuread_domains.aad_domains.domains[0].domain_name}"
+	display_name = "J. Doe"
+	given_name = "John"
+	surname = "Doe"
+	mail_nickname = "jdoe"
+	password = random_password.password.result
+	company_name = "evolvecyber"
+	country = "US"
+	city = "Chicago"
+	postal_code = 60018
+	state = "IL"
+	street_address = "312 n State"
+	office_location = "Wacker drive"
+	job_title = "DevOps"
+	mobile_phone = "31213232123"
+	department = "IT"
+}
