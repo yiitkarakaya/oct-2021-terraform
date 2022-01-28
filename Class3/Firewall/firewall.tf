bb4bd4ba45afb394d55ff77a97a3198134be4299
farrukh90 <farrukhsadykov@gmail.com>
2022-01-27 20:02:01 -0600

    added rule

--- Class3/Firewall/firewall.tf
+++ Class3/Firewall/firewall.tf
@@ -32,3 +32,60 @@ resource "azurerm_public_ip" "example" {
 	allocation_method = "Static"
 	sku = "Standard"
 }
+
+
+
+resource "azurerm_firewall" "example" {
+	name = "testfirewall"
+	location = azurerm_resource_group.example.location
+	resource_group_name = azurerm_resource_group.example.name
+	ip_configuration {
+		name = "configuration"
+		subnet_id = azurerm_subnet.example.id
+		public_ip_address_id = azurerm_public_ip.example.id
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+resource "azurerm_firewall_application_rule_collection" "example" {
+	name = "testcollection"
+	azure_firewall_name = azurerm_firewall.example.name
+	resource_group_name = azurerm_resource_group.example.name
+	priority = 100
+	action = "Allow"
+	rule {
+		name = "testrule"
+		source_addresses = [
+			"10.0.0.0/16",
+	]
+	target_fqdns = [
+		"*.google.com",
+	]
+	protocol {
+		port = "443"
+		type = "Https"
+		}
+	}
+}
