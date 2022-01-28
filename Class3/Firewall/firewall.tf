a7d6600583a54d1affa266d8f7e632fed8fb0c06
farrukh90 <farrukhsadykov@gmail.com>
2022-01-27 19:57:53 -0600

    added IP

---
+++ Class3/Firewall/firewall.tf
@@ -0,0 +1,38 @@
+
+provider "azurerm" {
+	features {}
+}
+
+resource "azurerm_resource_group" "example" {
+	name = "example-resources"
+	location = "West Europe"
+}
+
+
+
+resource "azurerm_virtual_network" "example" {
+	name = "testvnet"
+	address_space = ["10.0.0.0/16"]
+	location = azurerm_resource_group.example.location
+	resource_group_name = azurerm_resource_group.example.name
+}
+
+
+
+
+resource "azurerm_subnet" "example" {
+	name = "AzureFirewallSubnet"
+	resource_group_name = azurerm_resource_group.example.name
+	virtual_network_name = azurerm_virtual_network.example.name
+	address_prefixes = ["10.0.1.0/24"]
+}
+
+
+
+resource "azurerm_public_ip" "example" {
+	name = "testpip"
+	location = azurerm_resource_group.example.location
+	resource_group_name = azurerm_resource_group.example.name
+	allocation_method = "Static"
+	sku = "Standard"
+}
