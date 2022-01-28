64a7fca2bfaa05a171ac58b4c6f0c13c026173b9
farrukh90 <farrukhsadykov@gmail.com>
2022-01-27 19:35:16 -0600

    added storage account

--- Class3/StorageAccount/provider.tf
+++ Class3/StorageAccount/provider.tf
@@ -1,3 +1,32 @@
 provider "azurerm" {
 	features {}
 }
+
+
+resource "azurerm_resource_group" "example" {
+	name = "example-resources"
+	location = "West Europe"
+}
+
+resource "random_string" "random" {
+	length = 14
+	special = false
+	upper = false
+}
+
+
+
+resource "azurerm_storage_account" "example" {
+	name = "example${random_string.random.result}"
+	resource_group_name = azurerm_resource_group.example.name
+	location = azurerm_resource_group.example.location
+	account_tier = "Standard"
+	account_replication_type = "LRS"
+}
+
+
+resource "azurerm_storage_container" "example" {
+	name = "content"
+	storage_account_name = azurerm_storage_account.example.name
+	container_access_type = "private"
+}
