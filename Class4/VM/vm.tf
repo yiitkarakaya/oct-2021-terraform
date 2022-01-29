f752576a32813916fd18364ce9735d09c9f2bb79
farrukh90 <farrukhsadykov@gmail.com>
2022-01-29 20:43:51 +0000

    added vm

---
+++ Class4/VM/vm.tf
@@ -0,0 +1,28 @@
+resource "google_compute_instance" "vm_instance" {
+	name = var.vm_config["instance_name"]
+	machine_type = var.vm_config["machine_type"]
+	boot_disk {
+		initialize_params {
+			image = var.vm_config["image"]
+		}
+	}
+	network_interface {
+		network = "default"
+		access_config {
+		}
+	}
+	labels = var.labels
+	tags = [var.vm_config["network_tags"]]
+}
+
+resource "google_compute_firewall" "allow-http" {
+	name = var.vm_config["firewall_name"]
+	network = "default"
+	allow {
+		protocol = "tcp"
+		ports = ["80","22"]
+	}
+	source_tags = [var.vm_config["network_tags"]]
+}
+
+
