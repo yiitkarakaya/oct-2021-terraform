225ce54079f13bcdd17620c57ec53d75332422a1
farrukh90 <farrukhsadykov@gmail.com>
2022-02-16 01:21:01 +0000

    added aks

---
+++ Kubernetes/AKS/output.tf
@@ -0,0 +1,14 @@
+output "admin_host" {
+	value = module.aks.admin_host
+}
+output "admin_username" {
+	value = module.aks.admin_username
+}
+
+output "aks_id" {
+	value = module.aks.aks_id
+}
+
+output "location" {
+	value = module.aks.location
+}
