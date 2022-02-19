20c82a5e873d4288ff3db0c17c6c9350657740fb
farrukh90 <farrukhsadykov@gmail.com>
2022-02-19 20:46:30 +0000

    added vpcdeployer

---
+++ VPCDeployer.groovy
@@ -0,0 +1,23 @@
+node {
+	properties([
+		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+			pipelineTriggers([cron('H/15 * * * *')]),
+				parameters(
+					[string(defaultValue: 'apply', description: 'Should I build or destroy? ', name: 'TF_ACTION', trim: true),
+					choice(choices: [
+						'dev', 
+					], 
+					description: 'Which Environment should we deploy?', 
+					name: 'ENVIR')])])
+	stage("Clone a Repo"){
+		checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/jenkins-class.git']]])
+		}
+	stage("Build VPC"){
+		ws("${workspace}/AWS/VPC"){
+			sh "ENVIRONMENT=${ENVIR} make tf-fmt tf-init tf-plan tf-apply"
+			}
+		}
+	stage("Send Notification"){
+		echo "hello"
+	}
+}
